9ed992482abe98bc2164036ef79ee09983745072
NogueiraTNT <nogueirad345@gmail.com>
2026-03-29 15:03:50 -0300

    feat: nav menu and button exit sistem

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,4 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  signingOut = false;
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+  ) {}
+
+  async logout(): Promise<void> {
+    if (this.signingOut) {
+      return;
+    }
+    this.signingOut = true;
+    try {
+      await this.afAuth.signOut();
+      await this.router.navigate(['/login']);
+    } finally {
+      this.signingOut = false;
+    }
+  }
+}
